cc77bca0efd5a873c87cce955db5b95da8be2527
Cristopher <cristopher.emodesto@gmail.com>
2022-01-25 13:51:41 -0600

    Added Progress Input, Output with Logic

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
-  constructor() { }
+  progressOne: number = 5;
+  progressTwo: number = 5;
 
-  ngOnInit(): void {
+  get getPercentOne(){
+    return `${ this.progressOne }%`
   }
 
+  get getPercentTwo(){
+    return `${ this.progressTwo }%`
+  }
+
+  changeValueOne(event: any){
+    console.log(event)
+    this.progressOne = event;
+  }
+
+  changeValueTwo(event: any){
+    console.log(event)
+    this.progressTwo = event;
+  }
 }
