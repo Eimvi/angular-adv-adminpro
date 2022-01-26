0304b0a207642427d5fe7efcb3bf0043022731c1
Cristopher <cristopher.emodesto@gmail.com>
2022-01-26 13:27:37 -0600

    Added SidebarService

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [{
+    title: 'Dashboard',
+    icon: 'mdi mdi-gauge',
+    submenu: [
+      { title: 'Main', url:'' },
+      { title: 'Progress Bar', url: 'progress' },
+      { title: 'Charts', url: 'graphic' }
+    ]
+
+  }]
+  constructor() { }
+}
