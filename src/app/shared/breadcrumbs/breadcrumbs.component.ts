e8823e7d7b65fb9c39532cec107faa73eafd5108
Cristopher <cristopher.emodesto@gmail.com>
2022-01-27 10:07:55 -0600

    Added Observable

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  public title: string = '';
+  private titleSubs$: Subscription = new Subscription();
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.titleSubs$ = this.getRouteArguments()
+                           .subscribe(({title}) =>{
+                              this.title = title;
+                              document.title = `AdminPro - ${ title }`
+                            });
+   }
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
   }
 
+   getRouteArguments(){
+    return this.router.events
+    .pipe(
+      filter<any>((event: ActivationEnd) => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map((event:ActivationEnd) => event.snapshot.data)
+    );
+   }
 }
